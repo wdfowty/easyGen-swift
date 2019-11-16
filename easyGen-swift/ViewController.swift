ccb6a67d5eaf16c48a07ce03206e6732025ddc82
Sean Whitley <wdfowty@gmail.com>
2019-11-16 02:01:07 -0800

    Initial Commit

---
+++ easyGen-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  easyGen-swift
+//
+//  Created by Sean Whitley on 11/16/19.
+//  Copyright © 2019 Developed by Dubya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
